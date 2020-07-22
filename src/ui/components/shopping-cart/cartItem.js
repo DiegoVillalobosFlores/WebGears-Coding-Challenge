ac6e60a8bf6807e898f7b44d0d3e046b454f3d4d
Villalobos <diegovillalobos397@gmail.com>
2020-07-22 23:05:54 +0200

    Styled cart item

--- src/ui/components/shopping-cart/cartItem.js
+++ src/ui/components/shopping-cart/cartItem.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { shape, string, number, func } from 'prop-types';
 
+import '../../styles/shoppingCart.css';
+
 const CartItem = ({ product, onDelete }) => {
 	const { title, price, cart } = product;
 
 	return (
-		<div>
-			<p>{title}</p>
-			<p>{price}</p>
-			<p>Quantity:</p>
-			<p>{cart}</p>
-			<input type='button' value='X' onClick={onDelete}/>
+		<div className='CartItemContainer'>
+			<p className='CartItemLabel title'>{title}</p>
+			<p className='CartItemLabel price'>{price}</p>
+			<p className='CartItemLabel quantityLabel'>Quantity:</p>
+			<p className='CartItemLabel quantity'>{cart}</p>
+			<input className='CartItemButton' type='button' value='X' onClick={onDelete}/>
 		</div>
 	);
 };
