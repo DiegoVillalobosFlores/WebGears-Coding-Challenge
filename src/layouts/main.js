2c6ad217fe32ffb05de28e0bbf8c49d5984b324b
Villalobos <diegovillalobos397@gmail.com>
2020-07-22 17:27:49 +0200

    Added style for mainLayout.css

--- src/layouts/main.js
+++ src/layouts/main.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Inventory, ProductList, ShoppingCart} from '../components';
 
+import '../styles/mainLayout.css';
+
 const MainLayout = props => {
-	return (<div>
+	return (<div className='MainContainer'>
 		<ProductList/>
 		<ShoppingCart/>
 		<Inventory/>
