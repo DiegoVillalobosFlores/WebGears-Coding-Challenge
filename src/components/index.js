3dd0ebf318370af986aeeff5187c048bf012eb9e
Villalobos <diegovillalobos397@gmail.com>
2020-07-22 15:28:37 +0200

    Added base components for Inventory, ProductList and ShoppingCart

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Inventory from './inventory';
+import ProductList from './product-list';
+import ShoppingCart from './shopping-cart';
+
+export {
+	Inventory,
+	ProductList,
+	ShoppingCart
+};
---
+++ src/components/shopping-cart/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ShoppingCart = props => {
+	return (<div>
+    Cart
+	</div>);
+};
+
+export default ShoppingCart;
