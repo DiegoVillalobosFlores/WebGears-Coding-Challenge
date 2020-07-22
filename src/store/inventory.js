44f5dd7ce28a98d3c8f6a2065138369521878d6a
Villalobos <diegovillalobos397@gmail.com>
2020-07-22 18:22:41 +0200

    Fixed inventory.js reducer

--- src/store/inventory.js
+++ src/store/inventory.js
@@ -3,8 +3,23 @@ const PRODUCT_DELETE = 'PRODUCT_DELETE';
 
 export default {
 	reducer: {
-		PRODUCT_ADD: (inventory, action) => inventory.data.push(action.data),
-		PRODUCT_DELETE: (inventory, action) => inventory.data.filter(product => product.id !== action.data)
+		PRODUCT_ADD: (state, action) => ({
+			...state,
+			inventory: {
+				...state.inventory,
+				data: [...state.inventory.data, action.data]
+			}
+		}),
+		PRODUCT_DELETE: (state, action) => {
+			const { inventory } = state;
+			return ({
+				...state,
+				inventory: {
+					...inventory,
+					data: inventory.data.filter(product => product.id !== action.data)
+				}
+			});
+		}
 	},
 	actions: {
 		PRODUCT_ADD,
