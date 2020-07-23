dc4144f8a4209877c6b53c7e17f8da4f299cefbc
Villalobos <diegovillalobos397@gmail.com>
2020-07-23 23:21:47 +0200

    Optimized store by memoizing the reducer functions

--- src/store/inventory.js
+++ src/store/inventory.js
@@ -2,39 +2,42 @@ export const PRODUCT_ADD = 'PRODUCT_ADD';
 export const PRODUCT_DELETE = 'PRODUCT_DELETE';
 export const PRODUCT_UPDATE = 'PRODUCT_UPDATE';
 
-export const reducer = {
-	PRODUCT_ADD: (state, action) => ({
+const addProduct = (state, action) => ({
+	...state,
+	inventory: {
+		...state.inventory,
+		data: [...state.inventory.data, action.data]
+	}
+});
+
+const deleteProduct = (state, action) => ({
+	...state,
+	inventory: {
+		...state.inventory,
+		data: state.inventory.data.filter(product => product.id !== action.data)
+	}
+});
+
+const updateProduct = (state, action) => {
+	const { inventory } = state;
+
+	const itemIndex = inventory.data.findIndex(({ id }) => id === action.data.id);
+	if(itemIndex === -1) return state;
+
+	const newData = inventory.data;
+	newData[itemIndex] = action.data;
+
+	return ({
 		...state,
 		inventory: {
-			...state.inventory,
-			data: [...state.inventory.data, action.data]
+			...inventory,
+			data: newData
 		}
-	}),
-	PRODUCT_DELETE: (state, action) => {
-		const { inventory } = state;
-		return ({
-			...state,
-			inventory: {
-				...inventory,
-				data: inventory.data.filter(product => product.id !== action.data)
-			}
-		});
-	},
-	PRODUCT_UPDATE: (state, action) => {
-		const { inventory } = state;
+	});
+};
 
-		const itemIndex = inventory.data.findIndex(({ id }) => id === action.data.id);
-		if(itemIndex === -1) return state;
-
-		const newData = inventory.data;
-		newData[itemIndex] = action.data;
-
-		return ({
-			...state,
-			inventory: {
-				...inventory,
-				data: newData
-			}
-		});
-	}
+export const reducer = {
+	PRODUCT_ADD: addProduct,
+	PRODUCT_DELETE: deleteProduct,
+	PRODUCT_UPDATE: updateProduct,
 };
