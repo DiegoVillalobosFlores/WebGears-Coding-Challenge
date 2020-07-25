371242e848a10e7698afa49526ccb5cd371af82b
Villalobos <diegovillalobos397@gmail.com>
2020-07-26 00:51:03 +0200

    Added ADD_PRODUCT test

--- src/__tests__/store.test.js
+++ src/__tests__/store.test.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
 import '@testing-library/jest-dom/extend-expect';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import { StateProvider, store } from '../store';
+import { PRODUCT_ADD, PRODUCT_UPDATE, PRODUCT_DELETE } from '../store/inventory';
 
 it('inventory context', () => {
 	const TestComponent = () => {
@@ -28,4 +29,39 @@ it('inventory context', () => {
 	expect(rendered.getByText('First ID: ', { exact: false })).toHaveTextContent('0-unique-id');
 	expect(rendered.getByText('Second ID: ', { exact: false })).toHaveTextContent('1-unique-id');
 	expect(rendered.getByText('Third ID: ', { exact: false })).toHaveTextContent('2-unique-id');
+});
+
+it('add product', () => {
+	const TestComponent = () => {
+		const { state: globalState, dispatch } = useContext(store);
+		const { inventory } = globalState;
+		const { data } = inventory;
+
+		const testProduct = {
+			id: '4-unique-id',
+			title: 'test',
+		};
+
+		return <div>
+			{
+				data.length === 4 ?
+					<div>
+						<p>ID: {data[3].id}</p>
+						<p>Title: {data[3].title}</p>
+					</div>
+					:
+					<p>Waiting</p>
+			}
+			<button onClick={() => dispatch({ type: PRODUCT_ADD, data: testProduct })}>Add Product</button>
+		</div>;
+	};
+
+	const { getByText } = render(<StateProvider><TestComponent/></StateProvider>);
+
+	expect(getByText('Waiting')).toBeInTheDocument();
+
+	fireEvent.click(getByText('Add Product'));
+
+	expect(getByText('ID:', { exact: false })).toHaveTextContent('4-unique-id');
+	expect(getByText('Title:', { exact: false })).toHaveTextContent('test');
 });
